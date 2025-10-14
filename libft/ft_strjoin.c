eebf2727eab15dd37feb79c8550a96005ac39986
Eloi Berlinger <eloi.berling@protonmail.com>
2025-10-14 09:59:55 +0200

    ft_strjoin.c

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 09:49:04 by eberling          #+#    #+#             */
+/*   Updated: 2025/10/14 09:55:14 by eberling         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+	size_t	i_ret;
+	char	*ret;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ret = malloc(((s1_len + s2_len) + 1) * sizeof(char));
+	if(ret == NULL)
+		return (NULL);
+	i = -1;
+	while(++i < s1_len)
+		ret[i] = s1[i];
+	i_ret = i;
+	i = -1;
+	while(++i < s2_len)
+		ret[i_ret + i] = s2[i];
+	ret[i_ret + i] = '\0';
+	return (ret);
+}
