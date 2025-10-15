8b70326dc20672b7bcc585f98905cd0e1db4ba86
Eloi Berlinger <eloi.berling@protonmail.com>
2025-10-15 12:51:30 +0200

    DONE ft_putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,11 +6,35 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 11:20:25 by eberling          #+#    #+#             */
-/*   Updated: 2025/10/15 11:20:25 by eberling         ###   ########.fr       */
+/*   Updated: 2025/10/15 12:50:45 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putnbr_fd(int n, int fd)
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
 {
-	
+	ft_putstr_fd(ft_itoa(n), fd);
 }
+// #include <fcntl.h>  
+// #include <stdio.h>
+// int main(void)
+// {
+//     int fd;
+//     char *message = "Bravo, c'est ecrit dans le fichier !";
+//     fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+//     if (fd == -1)
+//     {
+//         perror("Erreur lors de l'ouverture du fichier");
+//         return (1);
+//     }
+//     printf("Debut de l'ecriture dans test.txt (FD: %d)...\n", fd);
+//     ft_putnbr_fd(50, fd);
+//     if (close(fd) == -1)
+//     {
+//         perror("Erreur lors de la fermeture du fichier");
+//         return (1);
+//     }
+//     printf("Termine. Veuillez verifier le contenu de 'test.txt'.\n");
+//     return (0);
+// }
