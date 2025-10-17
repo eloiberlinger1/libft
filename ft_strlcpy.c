eeee6a12f60073a11d41cbe8d2c422e3ef291237
Eloi Berlinger <eloi.berling@protonmail.com>
2025-10-17 11:27:29 +0200

    correction for edge case

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 17:55:15 by eberling          #+#    #+#             */
-/*   Updated: 2025/10/13 20:00:03 by eberling         ###   ########.fr       */
+/*   Updated: 2025/10/17 11:26:12 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
+	if (size == 0)
+		return (ft_strlen(src));
 	i = 0;
 	while (i < size - 1 && src[i] != '\0')
 	{
@@ -31,8 +33,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 
 // 	char test1[] = "adasdasd";
 // 	char test2[3];
+// 	char test3[3];
 
-// 	ft_strlcpy(test1, test2, 3);
+// 	printf("ft result :\n");
+// 	ft_strlcpy(test2, "aaa", 0);
 // 	printf("%s\n", test2);
+// 	printf("string.h result :\n");
+// 	strlcpy(test3, "aaa", 0);
+// 	printf("%s\n", test3);
 
 // }
