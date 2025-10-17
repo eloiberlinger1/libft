1969de7f52d0d696e85f87a5a832af1f17caa2e0
Eloi Berlinger <eloi.berling@protonmail.com>
2025-10-17 18:23:03 +0200

    Corrected strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 13:08:04 by eberling          #+#    #+#             */
-/*   Updated: 2025/10/17 16:00:32 by eberling         ###   ########.fr       */
+/*   Updated: 2025/10/17 18:22:25 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (str1[i] == str2[i] && i < n && str1[i] != '\0')
+	while (i < n && (str1[i] || str2[i]))
+	{
+		if ((unsigned char)str1[i] != (unsigned char)str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 		i++;
-	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	}
+	return (0);
 }
 // #include <stdio.h>
 // #include <string.h>
