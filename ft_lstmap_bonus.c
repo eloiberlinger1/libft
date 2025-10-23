a2b673abe99b28833c584b392b97b66f02c1e42f
Eloi Berlinger <eloi.berling@protonmail.com>
2025-10-23 15:17:16 +0200

    fixed lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 15:25:15 by eberling          #+#    #+#             */
-/*   Updated: 2025/10/18 17:25:58 by eberling         ###   ########.fr       */
+/*   Updated: 2025/10/23 15:17:04 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list_head;
 	t_list	*new_list;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list_head = NULL;
 	while (lst)
 	{
-		new_list = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_list = ft_lstnew(new_content);
 		if (!new_list)
 		{
-			ft_lstclear(&new_list, del);
+			del(new_content);
+			ft_lstclear(&new_list_head, del);
 			return (NULL);
 		}
 		if (new_list_head == NULL)
